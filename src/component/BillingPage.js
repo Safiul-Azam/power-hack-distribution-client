20d380571ddcb45383476cccdc63b77b5b17efdd
mdsafiulazam <mdriadkhan20@gmail.com>
2022-06-30 15:33:29 +0600

    installed react query for data fetching

--- src/component/BillingPage.js
+++ src/component/BillingPage.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import Loading from './Shared/Loading';
 
 const BillingPage = () => {
+    const {data:billingList, isLoading, refetch} = useQuery('billList',()=>fetch('http://localhost:5000/billingList').then(res => res.json()))
+
+    if(isLoading){
+       return <Loading></Loading>
+    }
     return (
         <div className='container'>
             <table class="table table-bordered table-striped">
@@ -15,12 +22,7 @@ const BillingPage = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                    </tr> 
+                    
                 </tbody>
             </table>
         </div>
---
+++ src/component/BillingSingle.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BillingSingle = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default BillingSingle;
